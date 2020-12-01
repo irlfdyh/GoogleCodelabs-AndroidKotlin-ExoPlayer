436bcc12932523797dc2ed36e7124c2729638c98
irshalfir <irshalfir@gmail.com>
2020-12-01 10:18:54 +0700

    add Media item for playing mp3 or mp4 media

--- player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -15,9 +15,17 @@
  */
 package com.example.exoplayer;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.util.Util;
 
 
 /**
@@ -25,10 +33,92 @@ import android.os.Bundle;
  */
 public class PlayerActivity extends AppCompatActivity {
 
+  private PlayerView playerView;
+  private SimpleExoPlayer player;
+
+  private boolean playWhenReady = true;
+  private int currentWindow = 0;
+  private long playbackPosition = 0;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_player);
+
+    playerView = findViewById(R.id.video_view);
+
+  }
+
+  private void initializePlayer() {
+    player = new SimpleExoPlayer.Builder(this).build();
+    playerView.setPlayer(player);
+
+    // Media item that playing a music
+//    MediaItem mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3));
+
+    //Media item that playing a video
+    MediaItem mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4));
+
+
+    player.setMediaItem(mediaItem);
+
+    // Initialize player when resuming
+    player.setPlayWhenReady(playWhenReady);
+    player.seekTo(currentWindow, playbackPosition);
+    player.prepare();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if (Util.SDK_INT >= 24) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    hideSystemUi();
+    if ((Util.SDK_INT < 24 || player == null)) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (Util.SDK_INT < 24) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (Util.SDK_INT >= 24) {
+      releasePlayer();
+    }
+  }
+
+  @SuppressWarnings("deprecation")
+  private void hideSystemUi() {
+    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+  }
+
+  private void releasePlayer() {
+    if (player != null) {
+      playWhenReady = player.getPlayWhenReady();
+      playbackPosition = player.getCurrentPosition();
+      currentWindow = player.getCurrentWindowIndex();
+      player.release();
+      player = null;
+    }
   }
 
 }
